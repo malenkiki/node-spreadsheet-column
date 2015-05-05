0e32b63963776ab9081c35f34b345efb084c9bc3
malenkiki <petit.michel@gmail.com>
2015-05-05 23:37:46 +0200

    Some additional UT

--- test.js
+++ test.js
@@ -5,65 +5,69 @@ var sc = new SpreadsheetColumn();
 
 describe('Converting from numeric index', function(){
     it('should return one-character length string', function(){
-        assert.equal(1, sc.fromInt(1).length);
-        assert.equal(1, sc.fromInt(17).length);
-        assert.equal(1, sc.fromInt(26).length);
+        assert.equal(sc.fromInt(1).length, 1);
+        assert.equal(sc.fromInt(17).length, 1);
+        assert.equal(sc.fromInt(26).length, 1);
     });
 
     it('should return two-characters length string', function(){
-        assert.equal(2, sc.fromInt(27).length);
-        assert.equal(2, sc.fromInt(300).length);
-        assert.equal(2, sc.fromInt(676).length);
+        assert.equal(sc.fromInt(27).length, 2);
+        assert.equal(sc.fromInt(300).length, 2);
+        assert.equal(sc.fromInt(676).length, 2);
     });
 
     //TODO I must enhance perf here : too slow!
     it('should return three-characters length string', function(){
-        assert.equal(3, sc.fromInt(900).length);
-        //assert.equal(3, sc.fromInt(12000).length);
-        //assert.equal(3, sc.fromInt(17576).length);
+        assert.equal(sc.fromInt(900).length, 3);
+        //assert.equal(sc.fromInt(12000).length, 3);
+        //assert.equal(sc.fromInt(17576).length, 3);
     });
 
     it('should return "A" string for integer 1', function(){
-        assert.equal("A", sc.fromInt(1));
+        assert.equal(sc.fromInt(1), 'A');
     });
 
     it('should return "B" string for integer 2', function(){
-        assert.equal("B", sc.fromInt(2));
+        assert.equal(sc.fromInt(2), "B");
     });
 
     it('should return "Y" string for integer 25', function(){
-        assert.equal("Y", sc.fromInt(25));
+        assert.equal(sc.fromInt(25), "Y");
     });
 
     it('should return "Z" string for integer 26', function(){
-        assert.equal("Z", sc.fromInt(26));
+        assert.equal(sc.fromInt(26), "Z");
     });
 
     it('should return "AA" string for integer 27', function(){
-        assert.equal("AA", sc.fromInt(27));
+        assert.equal(sc.fromInt(27), "AA");
     });
 
     it('should return "AB" string for integer 28', function(){
-        assert.equal("AB", sc.fromInt(28));
+        assert.equal(sc.fromInt(28), "AB");
     });
 
     it('should return "BI" string for integer 61', function(){
-        assert.equal("BI", sc.fromInt(61));
+        assert.equal(sc.fromInt(61), "BI");
     });
 
     it('should return "ZZ" string for integer 702', function(){
-        assert.equal("ZZ", sc.fromInt(702));
+        assert.equal(sc.fromInt(702), "ZZ");
     });
 
     it('should return "AAA" string for integer 703', function(){
-        assert.equal("AAA", sc.fromInt(703));
+        assert.equal(sc.fromInt(703), "AAA");
+    });
+
+    it('should return "AAB" string for integer 704', function(){
+        assert.equal(sc.fromInt(704), "AAB");
     });
 
     it('should return "ADO" string for integer 795', function(){
-        assert.equal("ADO", sc.fromInt(795));
+        assert.equal(sc.fromInt(795), "ADO");
     });
 
     it('should return "AEO" string for integer 821', function(){
-        assert.equal("AEO", sc.fromInt(821));
+        assert.equal(sc.fromInt(821), "AEO");
     });
 });
